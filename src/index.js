179df5a25ad902b113cc44ffed8a151679a3aa9b
Scott <wang.scottw@gmail.com>
2022-07-06 10:11:26 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    BrowerRouter,
+    Routes,
+    Route,
+    BrowserRouter,
+} from "react-router-dom";
+
+import { App } from './components'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <BrowserRouter>
+
+<App/>
+    
+    </BrowserRouter>
+);
